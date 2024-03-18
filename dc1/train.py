f13131bb07411e8b6676f3c951c8ad2c81c6e217
Dana Mirafzal <danadev94@gmail.com>
2024-03-18 19:02:09 +0100

    Cleaning up, introducing interface to use the model

--- dc1/train.py
+++ dc1/train.py
@@ -20,6 +20,7 @@ from datetime import datetime
 from pathlib import Path
 from typing import List
 import json
+from model import getModel
 
 # retrieve current time to label artifacts
 now = datetime.now()
@@ -39,10 +40,10 @@ data = {"model": model_file_name, "optimizer": optimizer_used, "description": de
 
 # ------------------------------------------------------------
 
-def saving_model_info(experiment_type_, data_):
-    path = f"../results/CNN-template/{experiment_type_}"
+def save_experiment(experiment_type_, data_):
+    path = f"results/CNN-template/{experiment_type_}"
     path_file = f'{path}/experiment_results.json'
-
+    json_data = []
     if not os.path.exists(path):
         os.makedirs(path)
         print("The new directory is created!")
@@ -62,15 +63,16 @@ def saving_model_info(experiment_type_, data_):
 
 def main(args: argparse.Namespace, activeloop: bool = True) -> None:
     # Load the train and test data set
-    train_dataset = ImageDataset(Path("../data/X_train.npy"), Path("../data/Y_train.npy"))
-    test_dataset = ImageDataset(Path("../data/X_test.npy"), Path("../data/Y_test.npy"))
+    train_dataset = ImageDataset(Path("data/X_train.npy"), Path("data/Y_train.npy"))
+    test_dataset = ImageDataset(Path("data/X_test.npy"), Path("data/Y_test.npy"))
+    # Get the value of lambda from arguments
+    weight_decay = args.weight_decay
 
-    # Load the Neural Net. NOTE: set number of distinct labels here
-    model = Net(n_classes=6)
+    model = getModel()
 
     # Initialize optimizer(s) and loss function(s)
     if optimizer_used == 'SGD':
-        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.1)
+        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.1, weight_decay=weight_decay)
     elif optimizer_used == 'Adam':
         optimizer = optim.Adam(model.parameters(), lr=0.001)
     else:
@@ -78,16 +80,13 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
     loss_function = nn.CrossEntropyLoss()
 
     # Initialize early stopping
-    early_stopper = EarlyStopper(patience=3, min_delta=10)
+    early_stopper = EarlyStopper(patience=3, min_delta=10,manual=True)
 
     # fetch epoch and batch count from arguments
     n_epochs = args.nb_epochs
     batch_size = args.batch_size
 
-    # IMPORTANT! Set this to True to see actual errors regarding
-    # the structure of your model (GPU acceleration hides them)!
-    # Also make sure you set this to False again for actual model training
-    # as training your model with GPU-acceleration (CUDA/MPS) is much faster.
+    # Set to false in order to enable GPU training acceleration
     DEBUG = False
 
     # Moving our model to the right device (CUDA will speed training up significantly!)
@@ -97,9 +96,8 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
         model.to(device)
         # Creating a summary of our model and its layers:
         summary(model, (1, 128, 128), device=device)
-    elif (
-            torch.backends.mps.is_available() and not DEBUG
-    ):  # PyTorch supports Apple Silicon GPU's from version 1.12
+
+    elif (torch.backends.mps.is_available() and not DEBUG):  # PyTorch supports Apple Silicon GPUs from version 1.12
         print("@@@ Apple silicon device enabled, training with Metal backend...")
         device = "mps"
         model.to(device)
@@ -109,16 +107,16 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
         # Creating a summary of our model and its layers:
         summary(model, (1, 128, 128), device=device)
 
-    # Lets now train and test our model for multiple epochs:
+    # Let's now train and test our model for multiple epochs:
     train_sampler = BatchSampler(
         batch_size=batch_size, dataset=train_dataset, balanced=args.balanced_batches
     )
-    test_sampler = BatchSampler(
-        batch_size=100, dataset=test_dataset, balanced=args.balanced_batches
-    )
+    test_sampler = BatchSampler(batch_size=100, dataset=test_dataset, balanced=args.balanced_batches)
 
     mean_losses_train: List[torch.Tensor] = []
     mean_losses_test: List[torch.Tensor] = []
+
+    earlyStopped = False
 
     for e in range(n_epochs):
         if activeloop:
@@ -142,10 +140,11 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
 
             # Early stopping
             if early_stopper.early_stop(mean_loss):
+                earlyStopped=True
                 print('Early stopping')
                 break
 
-            ### Plotting during training
+            #  Plotting during training
             plotext.clf()
             plotext.scatter(mean_losses_train, label="train")
             plotext.scatter(mean_losses_test, label="test")
@@ -161,22 +160,26 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
 
     # Saving the model
     torch.save(model.state_dict(), f'../{model_file_r_path}')
-    saving_model_info(experiment_type, data)
+    try:
+        save_experiment(experiment_type, data)
+    except:
+        print('!! Experiment not saved. Please record your experiment manually. !!')
 
     # Create plot of losses
     figure(figsize=(9, 10), dpi=80)
     fig, (ax1, ax2) = plt.subplots(2, sharex=True)
 
-    ax1.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_train], label="Train", color="blue")
-    ax2.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_test], label="Test", color="red")
-    fig.legend()
+    if not earlyStopped:
+        ax1.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_train], label="Train", color="blue")
+        ax2.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_test], label="Test", color="red")
+        fig.legend()
+        # Check if /artifacts/ subdir exists
+        if not Path("artifacts/").exists():
+            os.mkdir(Path("artifacts/"))
 
-    # Check if /artifacts/ subdir exists
-    if not Path("artifacts/").exists():
-        os.mkdir(Path("artifacts/"))
+        # save plot of losses
+        fig.savefig(Path("artifacts") / f"session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
 
-    # save plot of losses
-    fig.savefig(Path("artifacts") / f"session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
 
 
 if __name__ == "__main__":
@@ -192,6 +195,8 @@ if __name__ == "__main__":
         default=True,
         type=bool,
     )
+    parser.add_argument("--weight_decay", help="specifies the lambda value for L2 regularisation", default=0.1,
+                        type=float)
     args = parser.parse_args()
 
     main(args)
