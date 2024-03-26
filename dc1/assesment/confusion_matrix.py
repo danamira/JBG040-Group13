50900e231d0d3466ac217532994c5b12d4099fa5
BalinAmbra <a.balin@student.tue.nl>
2024-03-26 11:39:55 +0000

    Computed some different versions of the custom metric

--- dc1/assesment/confusion_matrix.py
+++ dc1/assesment/confusion_matrix.py
@@ -15,70 +15,93 @@ pred = data['pred']
 cm = confusion_matrix(true, pred)
 
 plt.figure(figsize=(12, 10))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", annot_kws={"size": 16}, xticklabels=class_labels, yticklabels=class_labels, cbar_kws={"label": "Scale"})
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", annot_kws={"size": 16}, xticklabels=class_labels,
+            yticklabels=class_labels, cbar_kws={"label": "Scale"})
 plt.xlabel('Predicted Label')
 plt.ylabel('True Label')
 plt.title('Confusion Matrix', weight='bold', size=16)
 plt.show();
 
-
 # Cost Matrix
 
+
 severity_weights = np.array([
-    [1, 1.1, 1.2, 1.6, 1.4, 1.3],
-    [1.7, 1, 1.1, 2, 1.3, 1.2],
-    [1.9, 1.8, 1, 1.8, 1.2, 1.1],
-    [1.1, 1.2, 1.3, 1, 1.5, 1.4],
-    [2.3, 2.2, 2.1, 2.4, 1, 2],
-    [2.1, 2, 1.9, 2.2, 1.1, 1]
+    [0, 0.85, 0.55, 0.8, 0.9, 0.6],
+    [3.55, 0, 3.65, 7, 4, 3.7],
+    [1.75, 2.15, 0, 3.4, 2.2, 1.9],
+    [0.05, 0.45, 0.15, 0, 0.5, 0.2],
+    [3.85, 4.25, 3.95, 7.6, 0, 4],
+    [4.65, 5.05, 4.75, 9.2, 5.1, 0]
 ])
+
+
+def min_max_normalize(matrix):
+    min_val = np.min(matrix)
+    max_val = np.max(matrix)
+    normalized_matrix = (matrix - min_val) / (max_val - min_val)
+    return normalized_matrix
+
+
+def sum_normalize(matrix):
+    row_sums = np.sum(matrix, axis=1)
+    normalized_matrix = matrix / row_sums[:, np.newaxis]
+    return normalized_matrix
+
+
+def matrix_sum_normalize(matrix):
+    norm_matrix = matrix / matrix.sum()
+    return norm_matrix
+
+
 # Computing the normalized matrix
-normalized_matrix = cm / cm.sum(axis=1, keepdims=True)
+normalized_matrix = matrix_sum_normalize(cm)
+# Computing normalised weights
+normalised_weights = matrix_sum_normalize(severity_weights)
 
-# Cmoputing the finalized matrix where we multiply it with the weights
-weighted_conf_matrix = normalized_matrix * severity_weights
+# Computing the finalized matrix where we multiply it with the weights
+weighted_conf_matrix_norm = normalized_matrix * normalised_weights
+weighted_conf_matrix = cm * severity_weights
+print(weighted_conf_matrix)
 
+# Computing norm of weighted_conf_matrix_norm
+norm_matrix = matrix_sum_normalize(weighted_conf_matrix_norm)
 
 # Computing precision and recall from cm
 precision = cm.diagonal() / cm.sum(axis=0)
 recall = cm.diagonal() / cm.sum(axis=1)
 
-# Computing precision and recall from weighted_conf_matrix
-precision_weight = weighted_conf_matrix.diagonal() / weighted_conf_matrix.sum(axis=0)
-recall_weight = weighted_conf_matrix.diagonal() / weighted_conf_matrix.sum(axis=1)
-
 # Handling potential division by zero issues
 precision = np.nan_to_num(precision)
 recall = np.nan_to_num(recall)
 
-precision_weight = np.nan_to_num(precision_weight)
-recall_weight = np.nan_to_num(recall_weight)
-
 # Compute F1 score for each class
 f1 = 2 * (precision * recall) / (precision + recall)
-
-# Compute F1 score for each class after adding weights
-f1_weight = 2 * (precision_weight * recall_weight) / (precision_weight + recall_weight)
-
 
 # Compute micro-average F1 score
 micro_f1 = f1_score(true, pred, average='micro')
 
-# Compute micro-average F1 score with weights
-micro_f1_weight = np.nan_to_num(f1_weight).mean()
-
 # Compute macro-average F1 score
-
 macro_f1 = f1_score(true, pred, average='macro')
 
-# Compute macro-average F1 score with weights
-macro_f1_weight = np.average(f1_weight, weights=np.unique(true, return_counts=True)[1])
+# Normalize custom metric
+cust_metric_norm = weighted_conf_matrix/weighted_conf_matrix.sum()
+print(cust_metric_norm)
+# MinMax  normalizing
+cust_metric_norm_minmax = min_max_normalize(weighted_conf_matrix)
+
+
+# Compute the custom score
+score_custom = cust_metric_norm.sum()
+score_custom_unnorm = weighted_conf_matrix.sum()
+norm_before_compute = weighted_conf_matrix_norm.sum()
+normalised_twice = norm_matrix.sum()
+minmax_score = cust_metric_norm_minmax.sum()
 
 print("Class-wise F1 Score:", f1)
 print("Micro-average F1 Score:", micro_f1)
 print("Macro-average F1 Score:", macro_f1)
-
-
-print("Class-wise F1 Score with weights added:", f1_weight)
-print("Micro-average F1 Score with weights added:", micro_f1_weight)
-print("Macro-average F1 Score with weights added:", macro_f1_weight)
+print("Custom score:", score_custom)
+print("Unnorm metric:", score_custom_unnorm)
+print("Norm before compute:", norm_before_compute)
+print("Normalised twice:", normalised_twice)
+print("MinMax norm version:", minmax_score)
