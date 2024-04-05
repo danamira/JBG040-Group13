752733ba4358b7e6c45c51cdd2bfe5bda9e921ff
Dana Mirafzal <danadev94@gmail.com>
2024-04-05 16:37:24 +0200

    Ethical model (hopefully)

--- dc1/train.py
+++ dc1/train.py
@@ -3,7 +3,6 @@ from dc1.batch_sampler import BatchSampler
 from dc1.image_dataset import ImageDataset
 from dc1.net import Net, Net_experiments, EarlyStopper
 from dc1.train_test import train_model, test_model
-from dc1.configurations import sampling_method, data_paths, data_path
 
 # Torch imports
 import torch
@@ -22,6 +21,15 @@ from pathlib import Path
 from typing import List
 import json
 from model import getModel
+import random
+from configurations import sampling_method
+import numpy as np
+
+# initialize seeds
+torch.manual_seed(42)
+random.seed(42)
+np.random.seed(42)
+torch.use_deterministic_algorithms(mode=True, warn_only=True)
 
 # retrieve current time to label artifacts
 now = datetime.now()
@@ -29,44 +37,46 @@ model_file_name = f"model_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}
 model_file_r_path = f"model_weights/{model_file_name}.txt"
 
 # Experiment Description
-architecture = "add configuration"
-experiment_type = "add configuration"
-optimizer_used = "Adam"  # Change depending on model used
-early_stopping = "add configuration"
-epochs = "add configuration"
-layers = "add configuration"
-convolutional_kernel = "add configuration"
-max_pooling_kernel = "add configuration"
-num_features_in_first_level = "add configuration"
-feature_decrement_factor = "add configuration"
-linear_layer = "add configuration"
-comment = "add configuration"
+architecture = "resnet"
+experiment_type = "seeds"
+optimizer_used = "SGD"  # Change depending on model used
+early_stopping = "false"
+epochs = "10"
+# layers = "4"
+# convolutional_kernel = "3"
+# max_pooling_kernel = "[2,2,2,2]"
+# num_features_in_first_level = "128"
+# feature_decrement_factor = "0.5"
+# linear_layer = "576"
+comment = "layer_list=[1,3,4,2,1], model trained with seed 42"
 
 # Aggregation of description
 data = {"model": model_file_name,
         "optimizer": optimizer_used,
         "early_stopping": early_stopping,
         "epochs": epochs,
-        "layers": layers,
-        "convolutional_kernel": convolutional_kernel,
-        "max_pooling_kernel": max_pooling_kernel,
-        "num_features_in_first_level": num_features_in_first_level,
-        "feature_decrement_factor": feature_decrement_factor,
-        "linear_layer": linear_layer,
+        # "layers": layers,
+        # "convolutional_kernel": convolutional_kernel,
+        # "max_pooling_kernel": max_pooling_kernel,
+        # "num_features_in_first_level": num_features_in_first_level,
+        # "feature_decrement_factor": feature_decrement_factor,
+        # "linear_layer": linear_layer,
         "comments": comment}
+
 
 # ------------------------------------------------------------
 
 def save_experiment(experiment_type_, data_, architecture_):
     path = f"results/{architecture_}/{experiment_type_}"
-    path_file = f'{path}/experiment_results.json'
+    path_file = f'{path}/experiment_results_seeds.json'
     json_data = []
     if not os.path.exists(path):
         os.makedirs(path)
         print("The new directory is created!")
 
         with open(path_file, "w") as write_file:
-            json.dump([data_], write_file, indent=4)
+            json_data.append(data_)
+            json.dump(json_data, write_file, indent=4)
 
     else:
         with open(path_file, 'r') as file:
@@ -80,13 +90,10 @@ def save_experiment(experiment_type_, data_, architecture_):
 
 def main(args: argparse.Namespace, activeloop: bool = True) -> None:
     # Load the train and test data set
-    train_dataset = ImageDataset(Path(f"data/{data_paths[data_path]}/X_train.npy"),
-                                 Path(f"data/{data_paths[data_path]}/Y_train.npy"),
-                                 sampling_method, train=True)
-    test_dataset = ImageDataset(Path(f"data/{data_paths[data_path]}/X_test.npy"),
-                                Path(f"data/{data_paths[data_path]}/Y_test.npy"),
-                                sampling_method, train=True)
-
+    train_dataset = ImageDataset(Path("data/X_train.npy"), Path("data/Y_train.npy"), sampling_method=sampling_method,
+                                 train=True)
+    test_dataset = ImageDataset(Path("data/X_test.npy"), Path("data/Y_test.npy"), sampling_method=sampling_method,
+                                train=False)
     # Get the value of lambda from arguments
     weight_decay = args.weight_decay
 
@@ -102,7 +109,7 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
     loss_function = nn.CrossEntropyLoss()
 
     # Initialize early stopping
-    early_stopper = EarlyStopper(patience=3, min_delta=10,manual=True)
+    early_stopper = EarlyStopper(patience=3, min_delta=10, manual=True)
 
     # fetch epoch and batch count from arguments
     n_epochs = args.nb_epochs
@@ -116,6 +123,10 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
         print("@@@ CUDA device found, enabling CUDA training...")
         device = "cuda"
         model.to(device)
+        # initialize seeds for CUDA
+        torch.cuda.manual_seed_all(42)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
         # Creating a summary of our model and its layers:
         summary(model, (1, 128, 128), device=device)
 
@@ -162,7 +173,7 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
 
             # Early stopping
             if early_stopper.early_stop(mean_loss):
-                earlyStopped=True
+                earlyStopped = True
                 print('Early stopping')
                 break
 
@@ -201,7 +212,6 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
 
         # save plot of losses
         fig.savefig(Path("artifacts") / f"session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
-
 
 
 if __name__ == "__main__":
